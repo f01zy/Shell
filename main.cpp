d426431da4b01e3d9a29efae65c3398886e2532c
f01zy <aminovali015@icloud.com>
2026-02-28 15:31:03 +0300

    added some commands

--- main.cpp
+++ main.cpp
@@ -4,15 +4,26 @@
 #include <vector>
 #include <wait.h>
 
+#define PATH_MAX 512
+
 using namespace std;
 
-int main() {
-  vector<string> args;
-  string curr;
-  string prompt;
+void changePath(string &path) {
+  char buffer[PATH_MAX];
+  if (getcwd(buffer, PATH_MAX)) {
+    path = buffer;
+  }
+}
 
+int main(int argc, char **argv) {
   while (1) {
-    cout << ">>> ";
+    vector<string> args;
+    string curr;
+    string prompt;
+    string path;
+
+    changePath(path);
+    cout << path << "$ ";
     if (!getline(cin, prompt)) {
       break;
     }
@@ -28,6 +39,7 @@ int main() {
         curr.push_back(ch);
       }
     }
+
     if (!curr.empty()) {
       args.push_back(curr);
     }
@@ -35,26 +47,37 @@ int main() {
       continue;
     }
 
+    if (args[0] == "cd") {
+      const char *newPath;
+      if (args.size() == 1) {
+        newPath = "/";
+      } else {
+        newPath = args[1].c_str();
+      }
+      if (chdir(newPath) != 0) {
+        cout << "Failed to change directory." << endl;
+      }
+      continue;
+    } else if (args[0] == "exit") {
+      exit(0);
+    }
+
     vector<char *> c_args;
     for (int i = 0; i < args.size(); i++) {
       c_args.push_back(const_cast<char *>(args[i].c_str()));
     }
+    c_args.push_back(nullptr);
 
     pid_t pid = fork();
     if (pid < 0) {
       cerr << "Cannot create process." << endl;
-      exit(0);
     } else if (pid == 0) {
       execvp(c_args[0], c_args.data());
       cerr << "Command not found or error." << endl;
       exit(1);
     } else {
       int statloc;
-      waitpid(-1, &statloc, 0);
+      waitpid(pid, &statloc, 0);
     }
-
-    args.clear();
-    curr.clear();
-    prompt.clear();
   }
 }
